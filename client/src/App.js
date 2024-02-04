06085264a0cfd6f0b23d57bfe2cc04cad30f3960
Pratim05 <pratimbera2017@gmail.com>
2024-02-04 19:22:51 +0530

    Initialization

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { ToastContainer } from "react-toastify";
+
+
+function App() {
+  return (
+   <BrowserRouter>
+    <Routes>
+   
+    </Routes>
+    <ToastContainer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
